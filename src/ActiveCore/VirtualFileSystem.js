96c3573995e73a66f766709a870e1e88c9ef47c1
Love-Kogasa <161302029+Love-Kogasa@users.noreply.github.com>
2025-05-31 22:18:04 +0800

    Add files via upload

--- src/ActiveCore/VirtualFileSystem.js
+++ src/ActiveCore/VirtualFileSystem.js
@@ -1,32 +1,32 @@
-var path = require( "path" )
+var p = require( "path" )
 module.exports = {
   _vDir: typeof $_vfs == "object" ? $_vfs : {},
   virtual: true,
   existsSync( path ){
-    return !!this._vDir[path]
+    return !!this._vDir[p.join(".", path)]
   },
   readFileSync( path ){
-    if( this._vDir[path] ) return this._vDir[path]
+    if( this._vDir[p.join(".", path)] ) return this._vDir[path]
     throw "No such file or directory"
   },
   writeFileSync( path, data ){
-    return this._vDir[path] = data
+    return this._vDir[p.join(".", path)] = data
   },
   mkdirSync( path ){
-    return this._vDir[path] = true
+    return this._vDir[p.join(".", path)] = true
   },
   appendFileSync( path ){
-    return this._vDir[path] += data
+    return this._vDir[p.join(".", path)] += data
   },
   readdirSync( path ){
     var output = []
     for( let filePath in this._vDir ){
-      if( path.dirname(filePath) == path ) output.push( filePath )
+      if( p.dirname(filePath) == path ) output.push( filePath )
     }
     return output
   },
   rmSync( path, options ){
-    var data = this._vDir[path]
+    var data = this._vDir[p.join(".", path)]
     if( options.recursive ){
       for( let p in this._vDir ){
         if( p.includes( path ) ) delete this._vDir[p]
